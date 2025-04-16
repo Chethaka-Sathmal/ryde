ad51f02026da7f951ab59f4106d19d692a758e97
Chethaka Sathmal <csathmal24@gmail.com>
2025-04-16 17:21:10 +0530

    added a custom font to the project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,39 @@
 import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
 import "./global.css";
 
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    'PlusJakartaSans-Bold': require('../assets/fonts/PlusJakartaSans-Bold.ttf'),
+    'PlusJakartaSans-BoldItalic': require('../assets/fonts/PlusJakartaSans-BoldItalic.ttf'),
+    'PlusJakartaSans-ExtraBold': require('../assets/fonts/PlusJakartaSans-ExtraBold.ttf'),
+    'PlusJakartaSans-ExtraBoldItalic': require('../assets/fonts/PlusJakartaSans-ExtraBoldItalic.ttf'),
+    'PlusJakartaSans-ExtraLight': require('../assets/fonts/PlusJakartaSans-ExtraLight.ttf'),
+    'PlusJakartaSans-ExtraLightItalic': require('../assets/fonts/PlusJakartaSans-ExtraLightItalic.ttf'),
+    'PlusJakartaSans-Italic': require('../assets/fonts/PlusJakartaSans-Italic.ttf'),
+    'PlusJakartaSans-Light': require('../assets/fonts/PlusJakartaSans-Light.ttf'),
+    'PlusJakartaSans-LightItalic': require('../assets/fonts/PlusJakartaSans-LightItalic.ttf'),
+    'PlusJakartaSans-Medium': require('../assets/fonts/PlusJakartaSans-Medium.ttf'),
+    'PlusJakartaSans-MediumItalic': require('../assets/fonts/PlusJakartaSans-MediumItalic.ttf'),
+    'PlusJakartaSans-Regular': require('../assets/fonts/PlusJakartaSans-Regular.ttf'),
+    'PlusJakartaSans-SemiBold': require('../assets/fonts/PlusJakartaSans-SemiBold.ttf'),
+    'PlusJakartaSans-SemiBoldItalic': require('../assets/fonts/PlusJakartaSans-SemiBoldItalic.ttf'),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'jakarta': ['PlusJakartaSans-Regular'],
+        'jakarta-bold': ['PlusJakartaSans-Bold'],
+        'jakarta-bold-italic': ['PlusJakartaSans-BoldItalic'],
+        'jakarta-extrabold': ['PlusJakartaSans-ExtraBold'],
+        'jakarta-extrabold-italic': ['PlusJakartaSans-ExtraBoldItalic'],
+        'jakarta-extralight': ['PlusJakartaSans-ExtraLight'],
+        'jakarta-extralight-italic': ['PlusJakartaSans-ExtraLightItalic'],
+        'jakarta-italic': ['PlusJakartaSans-Italic'],
+        'jakarta-light': ['PlusJakartaSans-Light'],
+        'jakarta-light-italic': ['PlusJakartaSans-LightItalic'],
+        'jakarta-medium': ['PlusJakartaSans-Medium'],
+        'jakarta-medium-italic': ['PlusJakartaSans-MediumItalic'],
+        'jakarta-semibold': ['PlusJakartaSans-SemiBold'],
+        'jakarta-semibold-italic': ['PlusJakartaSans-SemiBoldItalic'],
+      },
+    },
   },
   plugins: [],
 };
