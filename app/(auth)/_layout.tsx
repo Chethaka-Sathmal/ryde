56acfbc145f57278844a070a6fa71878860b8765
Chethaka Sathmal <csathmal24@gmail.com>
2025-04-16 18:08:35 +0530

    added auth pages

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,32 @@
+import { Stack } from "expo-router";
+
+export default function AuthLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: 'white' },
+        animation: 'slide_from_right',
+      }}
+    >
+      <Stack.Screen
+        name="welcome"
+        options={{
+          title: "Welcome",
+        }}
+      />
+      <Stack.Screen
+        name="signIn"
+        options={{
+          title: "Sign In",
+        }}
+      />
+      <Stack.Screen
+        name="signUp"
+        options={{
+          title: "Sign Up",
+        }}
+      />
+    </Stack>
+  );
+}
