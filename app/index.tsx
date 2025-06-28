d92b4c541773c95295509d3f11f5c70f3447aa72
Chethaka Sathmal <csathmal24@gmail.com>
2025-06-28 21:16:18 +0530

    testing

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, SafeAreaView } from "react-native";
 
 export default function Home() {
   return (
-    <SafeAreaView className="bg-foreground">
-      <Text style={{ fontFamily: "PlusJakartaSans_400Regular" }}>Hello</Text>
+    <SafeAreaView className="bg-primary">
+      <Text className="font-JakartaSemiBold">Hello</Text>
       <Text>Hello</Text>
     </SafeAreaView>
   );
